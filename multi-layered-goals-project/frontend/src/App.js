2e7ceea5a1c4d92a8c2e28dc969a697cc4fd1edb
Sachin Jain <sachin.jain.nsit@gmail.com>
2023-02-23 10:29:14 +0530

    trying to fix server_ip issue
    Signed-off-by: Sachin Jain <sachin.jain.nsit@gmail.com>

--- multi-layered-goals-project/frontend/src/App.js
+++ multi-layered-goals-project/frontend/src/App.js
@@ -17,7 +17,7 @@ function App() {
       try {
         console.log('SERVER_IP:' + process.env.APP_REACT_SERVER_IP);
 
-        const response = await fetch(`http://${process.env.SERVER_IP}/goals`);
+        const response = await fetch(`http://${process.env.REACT_APP_SERVER_IP}/goals`);
 
         const resData = await response.json();
 
@@ -43,7 +43,7 @@ function App() {
 
     try {
       console.log('SERVER_IP:' + process.env.APP_REACT_SERVER_IP);
-      const response = await fetch(`http://${process.env.SERVER_IP}/goals`, {
+      const response = await fetch(`http://${process.env.REACT_APP_SERVER_IP}/goals`, {
         method: 'POST',
         body: JSON.stringify({
           text: goalText,
@@ -83,7 +83,7 @@ function App() {
 
     try {
       console.log('SERVER_IP:' + process.env.APP_REACT_SERVER_IP);
-      const response = await fetch(`http://${process.env.SERVER_IP}/goals/` + goalId, {
+      const response = await fetch(`http://${process.env.REACT_APP_SERVER_IP}/goals/` + goalId, {
         method: 'DELETE',
       });
 

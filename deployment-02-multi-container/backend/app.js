9221e6e8e8aa226f3abf0f13788320290c7a28c5
Sachin Jain <sachin.jain.nsit@gmail.com>
2023-02-26 23:24:25 +0530

    allowing sufficient time for mongodb container to be started

--- deployment-02-multi-container/backend/app.js
+++ deployment-02-multi-container/backend/app.js
@@ -75,19 +75,21 @@ app.delete('/goals/:id', async (req, res) => {
   }
 });
 
-mongoose.connect(
-  `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}:27017/course-goals?authSource=admin`,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-  (err) => {
-    if (err) {
-      console.error('FAILED TO CONNECT TO MONGODB');
-      console.error(err);
-    } else {
-      console.log('CONNECTED TO MONGODB!!');
-      app.listen(80);
+setTimeout(() => {
+  mongoose.connect(
+    `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}:27017/course-goals?authSource=admin`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+    (err) => {
+      if (err) {
+        console.error('FAILED TO CONNECT TO MONGODB');
+        console.error(err);
+      } else {
+        console.log('CONNECTED TO MONGODB!!');
+        app.listen(80);
+      }
     }
-  }
-);
+  );    
+}, 120000);
